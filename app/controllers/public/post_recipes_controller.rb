3d65323cd8cfd93156977522e30cc0c7628cd556
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-08-07 09:55:10 +0000

    Add post-recipe

--- app/controllers/public/post_recipes_controller.rb
+++ app/controllers/public/post_recipes_controller.rb
@@ -1,31 +1,46 @@
 class Public::PostRecipesController < ApplicationController
 
   def index
-
+    @post_recipes = PostRecipe.all
   end
 
   def show
-
   end
 
   def new
-
-  end
-
-  def edit
-
+    @post_recipe = PostRecipe.new
   end
 
   def create
+    @post_recipe = PostRecipe.new(post_recipe_params)
+    @post_recipe.user_id = current_user.id
+    @post_recipe.save
+    redirect_to post_recipes_path
+  end
 
+  def edit
   end
 
   def update
-
   end
 
   def destroy
+  end
 
+private
+
+  def post_recipe_params
+    params.require(:post_recipe).permit(
+      :user_id,
+      :category_id,
+      :title,
+      :introduction,
+      :serving,
+      :post_status,
+      :image,
+      procedures_attributes: [:body, :_destroy],
+      ingredients_attributes: [:name, :amount, :_destroy]
+    )
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_05_081647) do
+ActiveRecord::Schema.define(version: 2023_08_07_070029) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,31 +53,48 @@ ActiveRecord::Schema.define(version: 2023_08_05_081647) do
   end
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
+    t.integer "post_recipes_id"
+    t.string "name"
+    t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "keeps", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_recipe_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "post_recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
+    t.string "title"
+    t.text "introduction"
+    t.integer "serving"
+    t.boolean "post_status", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "procedures", force: :cascade do |t|
+    t.integer "post_recipes_id"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "recipe_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_recipe_id"
+    t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -68,4 +113,6 @@ ActiveRecord::Schema.define(version: 2023_08_05_081647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
