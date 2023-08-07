74585c6b0befd4d35c475b72f406b9678f5e3152
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-08-07 01:11:01 +0000

    Add guest-login

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,5 @@
 class Public::UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
 
   def show
 
@@ -15,4 +16,14 @@ class Public::UsersController < ApplicationController
   def withdrawl
 
   end
+  
+  private
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.guest_user?
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end  
+  
 end
