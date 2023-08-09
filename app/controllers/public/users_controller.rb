3b5b9f3ed3ef3e471410554c15394b3515423b35
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-08-09 17:26:42 +0000

    user edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,28 +2,35 @@ class Public::UsersController < ApplicationController
   before_action :ensure_guest_user, only: [:edit]
 
   def show
-
+    @user = current_user
   end
 
   def edit
-
+    @user = current_user
   end
 
   def update
-
+    user = current_user
+    user.update(user_params)
+    redirect_to user_path
   end
 
   def withdrawl
 
   end
-  
+
+
   private
-  
+
+  def user_params
+    params.require(:user).permit(:email, :name, :profile_image, :is_deleted )
+  end
+
   def ensure_guest_user
     @user = User.find(params[:id])
     if @user.guest_user?
       redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
     end
-  end  
-  
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-         
+
+
   has_many :post_recipes, dependent: :destroy
   has_many :recipe_comments, dependent: :destroy
   has_many :keeps, dependent: :destroy
-  
+
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
   
   # ゲストログイン情報
   GUEST_USER_EMAIL = "guest@guest"
@@ -23,5 +32,5 @@ class User < ApplicationRecord
   def guest_user?
     email == GUEST_USER_EMAIL
   end
-  
+
 end
