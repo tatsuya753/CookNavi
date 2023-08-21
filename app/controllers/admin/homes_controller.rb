bfdbf5420f1d9f4cc86c01faeeb7131b16b1540d
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-08-21 12:04:38 +0000

    admin-pagenation

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,7 @@ class Admin::HomesController < ApplicationController
 
   def top
     @users = User.order(created_at: :desc)
+    @users = Kaminari.paginate_array(@users).page(params[:page]).per(10)
   end
 
 end
